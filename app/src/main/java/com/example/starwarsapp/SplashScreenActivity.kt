11e9319115c296d6128a61deeee69e81e5ea46eb
YawarValeriano <yawarvs@gmail.com>
2022-12-08 22:35:41 -0400

    Add splashscreen and app icons[

---
+++ app/src/main/java/com/example/starwarsapp/SplashScreenActivity.kt
@@ -0,0 +1,20 @@
+package com.example.starwarsapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        val mainIntent = Intent(this, MainActivity::class.java)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(mainIntent)
+            finish()
+        }, 2000)
+    }
+}
